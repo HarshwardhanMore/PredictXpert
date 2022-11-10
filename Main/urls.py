8a632722c9211ece729d2b8415491adf39cb553f
Harshwardhan More <87003053+HarshwardhanMore@users.noreply.github.com>
2022-11-10 13:10:33 +0530

    Final Commit

---
+++ Main/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from Main import views
+
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('',views.intro),
+    path('home',views.home2),
+    path('ml',views.ml),
+    path('seeavailabledatasets',views.seeavailabledatasets),
+    path('documentations',views.documentations),
+    path('rateus',views.rateus),
+    path('predict',views.predict),
+    path('manualpredict',views.manualpredict),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
